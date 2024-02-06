ace10645270fe5aa28526b2527f930661a31c677
Alex <alexss81@mail.ru>
2024-02-06 18:55:30 +0300

    adding react to the project

---
+++ portfolio-react/src/components/header/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./style.css"
+
+
+const Header = () => (
+    <header className="header">
+        <div className="header__wrapper">
+            <h1 className="header__title">
+                <strong>Hi, my name is <em>Alex</em></strong><br />
+                a frontend developer
+            </h1>
+            <div className="header__text">
+                <p>with passion for learning and creating.</p>
+            </div>
+            <a href="#!" className="btn">View GitHub profile</a>
+        </div>
+    </header>
+)
+ 
+export default Header;
